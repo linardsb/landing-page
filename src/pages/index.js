6dd2f10ab888d3eac60bf950ed56301e312f30a1
Linards Berzins <linardsberzins@gmail.com>
2020-04-09 11:39:40 +0100

    index

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]}/>
-    <h1>Hi people</h1>
+    <h1>Ugoki</h1>
     <p>Welcome to your new Gatsby site.</p>
 
   </Layout>
