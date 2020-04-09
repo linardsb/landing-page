840deb19754e29f7d0fafd2670016b8945947284
Linards Berzins <linardsberzins@gmail.com>
2020-04-09 10:37:55 +0100

    initial commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,17 +7,16 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+
 
 import "./layout.css"
 
-const Layout = ({ children }) => {
-  return (
+const Layout = ({ children }) => (
     <>
-   
+    {children}
     </>
   )
-}
+
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
