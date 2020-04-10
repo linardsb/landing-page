700154da62d62c82179b799a38894297875d8dea
Linards Berzins <linardsberzins@gmail.com>
2020-04-10 10:18:16 +0100

    added Cards to the index.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,10 +3,10 @@
  * with Gatsby's useStaticQuery component
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
-import React from "react"
-import PropTypes from "prop-types"
+import React from 'react'
+import PropTypes from 'prop-types'
 import Header from './Globals/header'
-import "./layout.css"
+import './layout.css'
 
 const Layout = ({ children }) => (
     <>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
-import React from "react"
-import Layout from "../components/layout"
+import React from 'react'
+import Layout from '../components/layout'
 import SEO from "../components/seo"
-import Wawe from '../components/Globals/Wawe';
+import Wawe from '../components/Globals/Wawe'
+import Card from '../components/Card'
+import { graphql } from 'gatsby'
 
 const IndexPage = () => (
   <Layout>
@@ -11,19 +13,68 @@ const IndexPage = () => (
         <h1> A simple way to stay focused and full of energy while you work</h1>
         <p>We can help you gain that extra quality time where it counts the most – with family and friends; to start a new hobby or take up on old pastime again; help get that promotion; make that business idea you thought of some time ago a reality. We do this using our own unique platform.</p>
         <div className="Logos">
-          <img src="../img/fasting.png" width="70" alt=""/>
-          <img src="../img/lemon.png" width="76" alt=""/>
-          <img src="../img/running.png" width="76" alt=""/>
-          <img src="../img/watch.png" width="50" alt=""/>
-          <img src="../img/strategy.png" width="70" alt=""/>
-          <img src="../img/worldwide.png" width="70" alt=""/>
-          <img src="../img/analysys.png" width="70" alt=""/>
-          <img src="../img/research.png" width="70" alt=""/>
+          <img src={require('../images/fasting.png')} width="70" alt=""/>
+          <img src={require('../images/lemon.png')} width="76" alt=""/>
+          <img src={require('../images/running.png')} width="76" alt=""/>
+          <img src={require('../images/watch.png')} width="50" alt=""/>
+          <img src={require('../images/strategy.png')} width="70" alt=""/>
+          <img src={require('../images/worldwide.png')} width="70" alt=""/>
+          <img src={require('../images/analysys.png')} width="70" alt=""/>
+          <img src={require('../images/research.png')} width="70" alt=""/>
         </div>
         <Wawe/>
       </div>
+    </div>
+    <div className="Cards"> 
+      <h2>Some of the goals of this product:</h2>
+      <p className="IntroText">Our mobile app will be designed as a well-being, health and human performance optimisation platform. A suggestive analytics and data modelling engine will be at the core of the product, to help and advise you on your success journey, with personalised approach to your body type, suggestive meal planning, physical performance, fitness the most natural way possible.</p>
+      <div className="CardGroup">
+        <Card
+        title="Time saver"
+        text="We'll help you to save your time"
+        image={require('../images/Planes2_v2.jpg')}
+         /> 
+        <Card
+        title="Excercise planner"
+        text="We'll work together to plan your fitness"
+        image={require('../images/Planes10_v2.jpg')} />
+        <Card
+        title="Fuel for the body"
+        text="You'll be able to choose a meal planner if you wish"
+        image={require('../images/Planes3_v2.jpg')} /> 
+        <Card
+        title="Track your progress"
+        text="Refer to the chart of your improvements"
+        image={require('../images/Planes4_v2.jpg')} /> 
+        <Card
+        title="Itermittent Fasting"
+        text="Science and body type based (IF) cycles"
+        image={require('../images/Planes5_v2.jpg')} /> 
+        <Card
+        title="Fitness video lessons"
+        text="Bite sized videos to stay in top form"
+        image={require('../images/Planes11_v2.jpg')} /> 
+        <Card
+        title="Social Network"
+        text="Communicate and interact with other members"
+        image={require('../images/Planes8_v2.jpg')} /> 
+        <Card
+        title="Research and science"
+        text="Research based articles and blog posts"
+        image={require('../images/Planes2_v2.jpg')} /> 
+        </div>
     </div>
   </Layout>
 )
 
 export default IndexPage
+
+export const query = graphql`
+  query SiteTitleQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
